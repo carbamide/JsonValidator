677f1af6d234ec56ccbe3bf72e9cef20fb7cf5d0
Josh Barrow <jbarrow@decisionpt.com>
2013-08-09 14:04:13 -0500

    code formatting

--- JSON Validator/NSString+JsonEscaping.m
+++ JSON Validator/NSString+JsonEscaping.m
@@ -10,8 +10,10 @@
 
 @implementation NSString (JsonEscaping)
 
-+ (NSString *)stringWithJSONString:(NSString *)jsonString {
++ (NSString *)stringWithJSONString:(NSString *)jsonString
+{
     NSUInteger len = [jsonString length];
+    
     if (len < 2 || [jsonString characterAtIndex:0] != '"' || [jsonString characterAtIndex:(len - 1)] != '"') {
         NSLog(@"%s: string does not begin and end with \" characters", __PRETTY_FUNCTION__);
         
@@ -19,14 +21,13 @@
     }
     
     NSMutableString *result = [[NSMutableString alloc] initWithCapacity:(len-2)];
+    
     NSUInteger limit = len - 1;
+    
     for (NSUInteger i = 1; i < limit; ++i) {
         unichar character = [jsonString characterAtIndex:i];
         
         if (character == '\\') {
-            
-            // Escape sequence
-            
             if (i == limit - 1) {
                 NSLog(@"%s: JSON string cannot end with single backslash", __PRETTY_FUNCTION__);
                 
@@ -38,48 +39,59 @@
             switch (nextCharacter) {
                 case 'b':
                     [result appendString:@"\b"];
+                    
                     break;
                     
                 case 'f':
                     [result appendString:@"\f"];
+                    
                     break;
                     
                 case 'n':
                     [result appendString:@"\n"];
+                    
                     break;
                     
                 case 'r':
                     [result appendString:@"\r"];
+                    
                     break;
                     
                 case 't':
                     [result appendString:@"\t"];
+                    
                     break;
                     
                 case 'u':
                     if ((i + 4) >= limit) {
                         NSLog(@"%s: insufficient characters remaining after \\u in JSON string", __PRETTY_FUNCTION__);
+                        
                         return nil;
                     }
                 {
                     NSString *hexdigits = [jsonString substringWithRange:NSMakeRange(i + 1, 4)];
+                    
                     i += 4;
+                    
                     NSScanner *scanner = [NSScanner scannerWithString:hexdigits];
+                    
                     unsigned int hexValue = 0;
+                    
                     if (![scanner scanHexInt:&hexValue]) {
                         NSLog(@"%s: invalid hex digits following \\u", __PRETTY_FUNCTION__);
                     }
+                    
                     [result appendFormat:@"%C", (unichar)hexValue];
                 }
                     break;
                     
                 default:
                     [result appendFormat:@"%C", nextCharacter];
+                    
                     break;
             }
         }
         else {
-            // No escape
             [result appendFormat:@"%C", character];
         }
     }
